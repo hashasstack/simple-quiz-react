ef82a6fd4dbf3ac1ffc6f6acf959f3e6d16d70a1
hashasstack <has.se@ecoinsoft.com>
2024-01-04 16:05:42 +0700

    Get quiz from opentdb api

--- src/context/DataProvider.js
+++ src/context/DataProvider.js
@@ -6,7 +6,15 @@ export const DataProvider = ({children}) => {
 const [showStart, setShowStart] = useState(true);
 const [showQuiz, setShowQuiz] = useState(false);
 
+function getQuiz() {
+    const url = 'https://opentdb.com/api.php?amount=10&category=18';
+    fetch(url)
+    .then((response) => response.json())
+    .then(data => console.log(data))
+}
+
 const startQuiz = () => {
+    getQuiz();
     setShowStart(false);
     setShowQuiz(true);
 }
